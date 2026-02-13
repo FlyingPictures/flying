38d77290688ee49eb2dbfe30d570bc74b9564c73
Víctor Rumaya <vrumaya@gmail.com>
2026-02-13 10:55:20 -0600

    v3.9

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,7 +1,5 @@
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
-import fs from "fs";
-import path from "path";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
@@ -10,20 +8,12 @@ export default getRequestConfig(async ({ requestLocale }) => {
     locale = routing.defaultLocale;
   }
 
-  const localePath = path.join(process.cwd(), "locales", locale);
-  const files = fs.readdirSync(localePath);
-
-  const messages = {};
-
-  for (const file of files) {
-    if (file.endsWith(".json")) {
-      const content = (await import(`../locales/${locale}/${file}`)).default;
-      Object.assign(messages, content);
-    }
-  }
+  const messages = await import(`../locales/${locale}.json`).then(
+    (module) => module.default
+  );
 
   return {
     locale,
-    messages
+    messages,
   };
 });
